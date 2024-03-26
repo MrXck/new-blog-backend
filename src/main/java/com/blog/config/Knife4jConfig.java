d3e24502bc014c17e454b48d85e82fbc64b223ce
MrXck <1971877559@qq.com>
2024-03-26 16:33:49 +0800

    修改knife4j配置类增加请求头设置

--- src/main/java/com/blog/config/Knife4jConfig.java
+++ src/main/java/com/blog/config/Knife4jConfig.java
@@ -1,12 +1,18 @@
 package com.blog.config;
 
+import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2WebMvc
@@ -18,7 +24,15 @@ public class Knife4jConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.blog.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(securitySchemes());
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        // 设置请求头信息
+        List<SecurityScheme> apiKeyList = new ArrayList<>();
+        apiKeyList.add(new ApiKey("Authorization", "Authorization", In.HEADER.toValue()));
+        return apiKeyList;
     }
 
 }
