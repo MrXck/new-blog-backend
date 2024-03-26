ceb80e995d51f6b2f582863a3509f6f8a7091459
MrXck <1971877559@qq.com>
2024-03-26 15:47:09 +0800

    新增文章错误枚举类

---
+++ src/main/java/com/blog/enums/article/ArticleErrorEnum.java
@@ -0,0 +1,18 @@
+package com.blog.enums.article;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ArticleErrorEnum {
+
+
+    ARTICLE_NOT_EXIST(10001, "文章不存在"),
+
+
+    IS_TOP_ERROR(10002, "置顶状态只能为 0 或 1");
+
+    private final Integer code;
+    private final String value;
+}
