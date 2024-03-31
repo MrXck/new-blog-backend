3c6808ca48826b1a9797175081af6f8234f14d8c
MrXck <1971877559@qq.com>
2024-03-31 17:46:51 +0800

    去除RoleResource无用接口增加根据RoleId获取接口

--- src/main/java/com/blog/controller/RoleResourceController.java
+++ src/main/java/com/blog/controller/RoleResourceController.java
@@ -1,16 +1,14 @@
 package com.blog.controller;
 
-import com.blog.model.dto.PageDTO;
-import com.blog.model.dto.roleResource.AddDTO;
 import com.blog.model.dto.roleResource.RoleResourceDTO;
-import com.blog.model.dto.roleResource.UpdateDTO;
 import com.blog.service.RoleResourceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "RoleResource相关")
 @RestController
@@ -20,40 +18,16 @@ public class RoleResourceController {
     @Autowired
     private RoleResourceService roleResourceService;
 
-    @ApiOperation("分页获取RoleResource")
-    @PostMapping("/page")
-    public RoleResourceDTO page(@RequestBody @Valid PageDTO dto) {
-        return roleResourceService.getByPage(dto);
-    }
-
-    @ApiOperation("添加RoleResource")
-    @PostMapping("/add")
-    public void add(@RequestBody @Valid AddDTO dto) {
-        roleResourceService.add(dto);
-    }
-
-    @ApiOperation("删除RoleResource")
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id) {
-        roleResourceService.deleteById(id);
-    }
-
-    @ApiOperation("修改RoleResource")
-    @PutMapping("/update")
-    public void update(@RequestBody @Valid UpdateDTO dto) {
-        roleResourceService.edit(dto);
-    }
-
     @ApiOperation("获取所有RoleResource")
     @GetMapping("/")
     public RoleResourceDTO list() {
         return roleResourceService.all();
     }
 
-    @ApiOperation("根据id获取RoleResource")
-    @GetMapping("/{id}")
-    public RoleResourceDTO getById(@PathVariable("id") Long id) {
-        return roleResourceService.findById(id);
+    @ApiOperation("根据角色id获取RoleResource")
+    @GetMapping("/{roleId}")
+    public RoleResourceDTO getByRoleIdId(@PathVariable("roleId") Long roleId) {
+        return roleResourceService.findByRoleId(roleId);
     }
 
 }
